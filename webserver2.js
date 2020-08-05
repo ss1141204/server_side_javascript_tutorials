dfeb8f65c348e8f8b6ee2945487d146ccc7b7b25
CsLee <ss1141204@gmail.com>
2020-08-06 02:38:57 +0900

    [fix] express - webserver2.js (webserver.js를 더 간결하게 작성)

--- webserver2.js
+++ webserver2.js
@@ -8,5 +8,7 @@ var server = http.createServer(function(req, res){
     res.end('Hello World\n');
 });
 server.listen(port, hostname, function(){
-  console.log('Server running at http://${hostname}:${port}\');
+  console.log('Server running at http://${hostname}:${port}/');
 });
+
+//webserver.js 의 코드를 더욱 간결하게 작성함
